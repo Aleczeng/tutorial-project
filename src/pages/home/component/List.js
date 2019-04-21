e37e54b100514018020ce980ddc8db274b23918f
AlecZeng <zxh93321@gmail.com>
2019-04-21 18:27:27 +1200

    article list

--- src/pages/home/component/List.js
+++ src/pages/home/component/List.js
@@ -1,9 +1,34 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
+import {connect} from "react-redux";
 
 class List extends Component {
     render() {
-        return <h1>List</h1>
+        return (
+            <Fragment>
+                {this.getArticleList()}
+            </Fragment>
+        )
+    }
+
+    getArticleList = () => {
+        return (
+            this.props.articleList.map(article => {
+                return (
+                    <div className='article-list' key={article.get('id')}>
+                        <div className='article-content'>
+                            <h3>{article.get('title')}</h3>
+                            <p>{article.get('content')}</p>
+                        </div>
+                        <img src={require('../../../assets/images/topic-img1.png')} alt='pic'/>
+                    </div>
+                )
+            })
+        )
     }
 }
 
-export default List
+const mapState = (state) => ({
+    articleList: state.home.get('articleList')
+});
+
+export default connect(mapState, null)(List)
