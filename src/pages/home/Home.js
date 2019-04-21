0e9986a39e7759f7b07e9b0aec55fe0ca1139757
AlecZeng <zxh93321@gmail.com>
2019-04-21 19:12:16 +1200

    get home content from api

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
 import {connect} from "react-redux";
 import HomeUI from "./HomeUI";
+import {getHomeContent} from "./store/action";
 
 class Home extends Component {
     render() {
         return <HomeUI/>
+    }
+
+    componentDidMount() {
+        this.props.getHomeContent();
     }
 }
 
@@ -12,8 +17,10 @@ const mapStateToProps = (state) => {
     return {}
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {}
-};
+const mapDispatchToProps = (dispatch) => ({
+    getHomeContent() {
+        dispatch(getHomeContent());
+    }
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/pages/home/store/action.js
+++ src/pages/home/store/action.js
@@ -1,1 +1,19 @@
+import axios from "axios";
 
+const initHomeContent = (value) => ({
+    type: INIT_HOME_CONTENT,
+    value
+});
+
+export const INIT_HOME_CONTENT = 'INIT_HOME_CONTENT';
+
+export const getHomeContent = () => {
+    return (dispatch) => {
+        axios.get('/api/home.json').then(resp => {
+            console.log(resp.data);
+            dispatch(initHomeContent(resp.data));
+        }).catch(e => {
+            console.log(e);
+        })
+    }
+};
